71b57d0cfee2ed4decb0352d95b11ab73e3c983d
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2025-04-12 21:40:49 +0300

    Update admin.py

--- reascon_backend/reascon_backend/contacts/admin.py
+++ reascon_backend/reascon_backend/contacts/admin.py
@@ -2,12 +2,6 @@
 from django.contrib import admin
 from .models import Contact, ProjectInquiry
 
-@admin.register(Contact)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'contact_type', 'submitted_at')
-    list_filter = ('contact_type',)
-    search_fields = ('name', 'email', 'subject', 'message')
-
 @admin.register(ProjectInquiry)
 class ProjectInquiryAdmin(admin.ModelAdmin):
     list_display = ( 'name', 'email', 'submitted_at')
